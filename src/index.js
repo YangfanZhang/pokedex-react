3f3fcd292d05365b8e6a82dc5a338b608fd441ee
YangfanZhang <yf_chang@icloud.com>
2020-07-18 15:06:45 +1000

    Added dependencies

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
+const browserHistory = createBrowserHistory();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router history={browserHistory}>
+      <App />
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+serviceWorker.unregister();
