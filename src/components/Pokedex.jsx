4105597c9cf125908ce0d2ea9796b88363d6560e
YangfanZhang <yf_chang@icloud.com>
2020-07-18 18:10:55 +1000

    Added pokedex container

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -2,21 +2,30 @@ import React from "react";
 import Header from "./Header";
 import PokemonCard from "./PokemonCard";
 import { makeStyles } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core";
 
 const useStyles = makeStyles({
-    pokedexContainer: {
-        paddingTop: "20px",
-        paddingLeft: "50px",
-        paddingRight: "50px",
-    }
+  pokedexContainer: {
+    paddingTop: "20px",
+    paddingLeft: "50px",
+    paddingRight: "50px",
+  },
 });
 
-
 function Pokedex() {
+  const classes = useStyles();
   return (
     <div>
       <Header />
-      <h1>This is the pokedex page</h1>
+      <Grid container spacing={2} className={classes.pokedexContainer}>
+        <PokemonCard />
+        <PokemonCard />
+        <PokemonCard />
+        <PokemonCard />
+        <PokemonCard />
+        <PokemonCard />
+        <PokemonCard />
+      </Grid>
     </div>
   );
 }
