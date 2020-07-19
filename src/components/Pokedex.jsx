5cf8440eab67d550cf5cd34736420470cf487e67
YangfanZhang <yf_chang@icloud.com>
2020-07-19 13:17:03 +1000

    Added pokemon types, rendered all needed info on pokemonCards

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import PokemonCard from "./PokemonCard";
 import { makeStyles } from "@material-ui/core/styles";
-import { Grid, CircularProgress, CardMedia} from "@material-ui/core";
-import mockData from "./mockData";
+import { Grid, CircularProgress } from "@material-ui/core";
+import axios from "axios";
 
 const useStyles = makeStyles({
   pokedexContainer: {
@@ -15,14 +15,41 @@ const useStyles = makeStyles({
 
 function Pokedex() {
   const classes = useStyles();
-  const [pokemonData, setPokemonData] = useState(mockData);
+  const [pokemonsData, setPokemonsData] = useState({});
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=12")
+      .then(function (response) {
+        const { data } = response;
+        const { results } = data;
+        const newPokemonsData = {};
+
+        results.forEach((pokemon, index) => {
+          newPokemonsData[index + 1] = {
+            id: index + 1,
+            name: pokemon.name,
+            sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+              index + 1
+            }.png`,
+          };
+        });
+        setPokemonsData(newPokemonsData);
+      });
+  }, []);
 
   return (
     <div>
       <Header />
-      {pokemonData ? (
+      {pokemonsData ? (
         <Grid container spacing={2} className={classes.pokedexContainer}>
-         {Object.keys(pokemonData).map(pokemonId => {return <PokemonCard pokemonId={pokemonId}/>})}
+          {Object.keys(pokemonsData).map((pokemonId) => {
+            return (
+              <PokemonCard
+                pokemonId={pokemonId}
+                pokemonData={pokemonsData[pokemonId]}
+              />
+            );
+          })}
         </Grid>
       ) : (
         <CircularProgress />
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,44 +1,64 @@
-import React, { useState } from "react";
-import { Grid, Card, CardContent, CardMedia, Typography } from "@material-ui/core";
-import mockData from "./mockData";
+import React, { useState, useEffect } from "react";
+import {
+  Grid,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import axios from "axios";
+import Chip from "@material-ui/core/Chip";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  cardMedia:{
-      width: "130px",
-      height: "130px",
-      margin:"auto",
+  cardMedia: {
+    width: "130px",
+    height: "130px",
+    margin: "auto",
   },
-  cardContent:{
+  cardContent: {
     textAlign: "center",
-  }
+  },
 }));
-// const toFirstCharUppercase = name =>
-//   name.charAt(0).toUpperCase() + name.slice(1);
-function toFirstCharUppercase(text){
-    return (text.charAt(0).toUpperCase() + text.slice(1));
+
+function toFirstCharUppercase(text) {
+  return text.charAt(0).toUpperCase() + text.slice(1);
 }
 
 function PokemonCard(props) {
   const classes = useStyles();
-  const [pokemonData, setPokemonData] = useState(mockData);
-  const {
-    name,
-    id,
-    sprites: { front_default },
-  } = pokemonData[props.pokemonId];
+  const pokemonData = props.pokemonData;
+  const [pokemonTypes, setPokemonTypes] = useState([]);
+  useEffect(() => {
+  axios
+    .get(`https://pokeapi.co/api/v2/pokemon/${props.pokemonId + 1}`)
+    .then(function (response) {
+      const { data } = response;
+      const { types } = data;
+      const newTypes = [];
+      types.forEach((type) => {
+        const {
+          type: { name },
+        } = type;
+        newTypes.push(name);
+      });
+      setPokemonTypes(newTypes);
+    });
+});
+
   return (
     <Grid item xs={4} key={props.pokemonId}>
       <Card>
-        <CardMedia 
-        className={classes.cardMedia}
-            image={front_default}
-        />
+        <CardMedia className={classes.cardMedia} image={pokemonData.sprite} />
         <CardContent className={classes.cardContent}>
-            <Typography >{`${id}. ${toFirstCharUppercase(name)}`}</Typography>
+          <Typography>{`${pokemonData.id}`}</Typography>
+          <Typography>{`${toFirstCharUppercase(pokemonData.name)}`}</Typography>
+          {pokemonTypes.map((pokemonType) => {
+            return <Chip label = {`${pokemonType}`} />;
+          })}
         </CardContent>
       </Card>
     </Grid>
