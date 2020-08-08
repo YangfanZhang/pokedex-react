dddbe1098ae1eebae788853a379e23291bca054f
YangfanZhang <yf_chang@icloud.com>
2020-08-08 22:24:47 +1000

    Added react-query-devtools

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import Pokedex from "./Pokedex.jsx";
 import Party from "./Party.jsx";
 import {Route, Switch} from "react-router-dom";
+import {ReactQueryDevtools} from "react-query-devtools";
 
 function App() {
   return (
+    <>
     <Switch>
       <Route exact path="/" render={(props) => <Pokedex {...props}/>}/>
       <Route exact path="/pokedex" render={(props) => <Pokedex {...props}/>}/>
       <Route exact path="/party" render={(props) => <Party {...props}/>}/>
     </Switch>
+    <ReactQueryDevtools initialIsOpen={false} />
+    </>
   );
 }
-
 export default App;
