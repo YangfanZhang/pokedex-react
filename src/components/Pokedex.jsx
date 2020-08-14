f22ed2a0621b2787218b687e9fe08560fa47aa35
YangfanZhang <yf_chang@icloud.com>
2020-08-14 23:38:09 +1000

    Some clean up

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,37 +1,9 @@
 import React, { useState} from "react";
 import Header from "./Header";
-import { makeStyles } from "@material-ui/core/styles";
 import PokemonList from "./PokemonList";
 
-const useStyles = makeStyles({
-  pokedexContainer: {
-    paddingTop: "20px",
-    paddingLeft: "50px",
-    paddingRight: "50px",
-    alignSelf: "center",
-  },
-  pokeGrid: {
-    marginLeft: "10%",
-    marginRight: "10%",
-    textAlign: "center",
-    display: "grid",
-    width: "800px",
-    height: "800px",
-    margin: "0 auto",
-    overflow: "auto",
-  },
-});
-
 function Pokedex() {
-  // const classes = useStyles();
   const [partyMember, setpartyMember] = useState([]);
-
-  // const handleScroll = (event) => {
-  //   const { scrollTop, clientHeight, scrollHeight } = event.currentTarget;
-  //   if (scrollHeight - scrollTop === clientHeight) {
-  //     setCurrentPage((prev) => prev + 1);
-  //   }
-  // };
 
   function AddToParty(newId) {
     setpartyMember((prevMembers) => {
